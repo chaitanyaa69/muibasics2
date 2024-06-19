1d6c70c06a491ef76a54f13e9eab70473a9b6b11
Chaitanya Reddy <reddy03042004@gmail.com>
2024-06-19 11:51:00 +0530

    integrated chatbot

--- src/Api-Config.jsx
+++ src/Api-Config.jsx
@@ -1,3 +1,3 @@
-export const loginUrl = 'https://64c5a21c-9e15-4b1b-80b7-986d02c91f5a.mock.pstmn.io'
-//https://api-dev.hdgtl.xyz/login'
+export const loginUrl = 'https://api-dev.hdgtl.xyz/login'
+//'
 //https://64c5a21c-9e15-4b1b-80b7-986d02c91f5a.mock.pstmn.io
